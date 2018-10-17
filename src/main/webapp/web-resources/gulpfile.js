694adf77d41e27af561627fa399fe8d6801739fa
Vladislav <vladislav.shvorobuk@gmail.com>
2018-10-17 22:35:22 +0300

    add gulp soursmaps, config gulpfile

--- src/main/webapp/web-resources/gulpfile.js
+++ src/main/webapp/web-resources/gulpfile.js
@@ -1,11 +1,14 @@
 "use strict";
 const gulp = require("gulp");
 const sass = require("gulp-sass");
+const sourcemaps = require("gulp-sourcemaps");
 const browserSync = require("browser-sync");
 
 gulp.task("sass", function () {
    return gulp.src("./scss/**/*.scss")
-       .pipe(sass())
+       .pipe(sourcemaps.init())
+       .pipe(sass().on("error", sass.logError))
+       .pipe(sourcemaps.write())
        .pipe(gulp.dest("./css"));
 });
 gulp.task("watch:sass", function () {
